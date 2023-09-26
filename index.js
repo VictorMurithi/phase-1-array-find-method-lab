314c15d147d7a7074dd80f918a451243798b1776
victor <victor.murithi@student.moringaschool.com>
2023-09-26 10:24:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record){
+    const wins = record.find(play=>play.result === "W");
+    if (wins){
+        console.log("year",wins.year)
+        return wins.year; 
+    }else{
+        return undefined;
+    }
+};
